bf9d48b4cf20f0eacf8af5110dbee0940b72a234
David Garrett <carpedavid@gmail.com>
2024-02-11 14:45:47 -0500

    rewriting

---
+++ necromancer_data.js
@@ -0,0 +1,125 @@
+gen_data['necromancer'] = [
+    'Your <strong>focus</strong> is {a} [focus].'
+]
+
+gen_data['homeworld'] = {
+    '1': 'Maono',
+    '2-3': 'Almasi',
+    Jangwa
+    Msituni ^2
+    Barafu
+    Nyumbani ^3
+    Mwewe
+    Kivuli
+}
+
+gen_data['voice'] = [
+    'you were physically attacked',
+    'your loved one suddenly passed away',
+    'you witnessed an unspeakable act of violence',
+    'you contracted a debilitating illness',
+    'your most closely held secret was publicly revealed',
+    'your financial accounts were hacked'
+]
+
+gen_data['cyberware'] = [
+    'telescopic irises',
+    'illuminated skin',
+    'mechanical limb',
+    'titanium exoskeleton',
+    'storage hatches',
+    'mounting points'
+]
+
+gen_data['enhancements'] = [
+    'limb graft',
+    'melanin/chlorophyl replacement',
+    'prehensile tail',
+    'bioluminescent hair',
+    'horns',
+    'fungal skin infusion'
+]
+
+gen_data['openness'] = [
+    'close-minded',
+    'rigid',
+    'knowledgable',
+    'curious',
+    'artistic',
+    'adventurous'
+]
+
+gen_data['conscientiousness'] = [
+    'irresponsible',
+    'disorganized',
+    'effective',
+    'reliable',
+    'careful',
+    'serious'
+]
+
+gen_data['extraversion'] = [
+    'solitary',
+    'reserved',
+    'sociable',
+    'energetic',
+    'assertive',
+    'gregarious'
+]
+
+gen_data['agreeableness'] = [
+    'manipulative',
+    'callous',
+    'gruff',
+    'polite',
+    'thoughtful',
+    'generous'
+]
+
+gen_data['stability'] = [
+    'volatile',
+    'jumpy',
+    'pessimistic',
+    'optimistic',
+    'resilient',
+    'carefree'
+]
+
+gen_data['focus'] = [
+    'a length of rope, frayed and broken',
+    'a straight razor, stained with blood',
+    'an empty bottle of pills',
+    'a bullet casing, slightly warm to the touch',
+    'a keycard that grants access to the roof of a tall building',
+    'a train ticket, unused',
+    'a rat skull that squeaks happily when held',
+    'a shark\'s tooth that smells of the sea',
+    'a dog collar that floats around excitedly if left unattended',
+    'a rabbit\'s foot that twitches occasionally',
+    'a horse shoe, well worn',
+    'a feather that is immune to gravity',
+    'a hologram of someone you know very well',
+    'a locket engraved with the initials of someone you\'ve never met',
+    'a book of poems, well-read',
+    'a hymnal in mint condition',
+    'a bottle of perfume, half used',
+    'a satchel filled with floral potpourri',
+    'a wooden animal, hand-carved',
+    'a child\'s doll, hand-sewn',
+    'a porcelain figurine of a dancer',
+    'a bronze statuette of a hunter',
+    'a crystal pendant of a bird',
+    'a figurine of an unknown animal, carved from bone',
+    'a pocket watch that is perpetually wound',
+    'a fountain pen filled with blood red ink',
+    'a lighter that is eternally fueled',
+    'a notebook, black',
+    'a pocket knife, sharp',
+    'a paperweight in the form of an onyx pyramid',
+    'a trumpet that only plays jazz',
+    'a pair of headphones with kickin\' bass',
+    'a guitar, tuned perfectly',
+    'a golden violin',
+    'a drum made from skin of dubious provenance',
+    'a harmonica that plays itself'
+]
